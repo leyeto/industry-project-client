6f7070d906867adf6f971bbbc700bc4ed5cfd92b
Adeleye Ifaturoti <adeleyeifaturoti@yahoo.com>
2022-07-28 23:23:20 +0100

    rough footer

---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import searchWrench from "../../assets/icons/search-icon-menu-bar.svg";
+import profileIcon from "../../assets/icons/profile-icon.svg";
+
+import "./Footer.scss";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer__search-icon">
+        <img src={searchWrench} alt="search-icon-wrench" />
+        <h4>Search</h4>
+      </div>
+      <div className="footer__profile-icon">
+        <img src={profileIcon} alt="search-icon-wrench" />
+        <h4>profile</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
