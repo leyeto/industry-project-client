053ee5ac23511c9edfde7999deca9b1505e65cf0
Adeleye Ifaturoti <adeleyeifaturoti@yahoo.com>
2022-07-28 21:22:02 +0100

    basic partial files added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route to="/" component={App} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
