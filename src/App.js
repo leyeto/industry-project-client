b85ca9018c9ea19853e9608ba3e20cf9db6bd33d
Adeleye Ifaturoti <adeleyeifaturoti@yahoo.com>
2022-07-28 22:53:43 +0100

    assets added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Route, Switch } from "react-router-dom";
 import "./App.scss";
 import FixitHeader from "./component/FixItHeader/FixitHeader";
 
-
 function App() {
   return (
     <div className="App">
