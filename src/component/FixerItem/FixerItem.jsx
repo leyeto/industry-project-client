643ebccacf3821ead8819351f1c0a0e5f76b366d
Joe Shandley <josephshandley@gmail.com>
2022-07-29 10:39:39 +0100

    Add commented star code

--- src/component/FixerItem/FixerItem.jsx
+++ src/component/FixerItem/FixerItem.jsx
@@ -6,6 +6,8 @@ import image1 from "../../assets/images/profile-image.jpg";
 import image2 from "../../assets/images/blonde.jpg";
 import image3 from "../../assets/images/omid-armin-UVx7Xx_b4a0-unsplash.jpg";
 import fullStar from "../../assets/icons/full-star.svg";
+import emptyStar from "../../assets/icons/empty-star.svg";
+const ReactRating = require("react-rating"); //If const doesn't work IMPORT will, didn't get to test require but tested import on App.js
 
 export default function FixerItem({ fixer }) {
   const distance = Math.round(fixer.distance * 100) / 100;
@@ -37,6 +39,13 @@ export default function FixerItem({ fixer }) {
           <h3 className="fixer__name">{fixer.name}</h3>
           <p className="fixer__distance">{`${distance}km away`}</p>
           <p className="fixer__description">{fixer.description}</p>
+          {/* <div className="fixer__rating">
+            <ReactRating
+              initialRating={fixer.stars}
+              emptySymbol={<img src={emptyStar} className="icon" />}
+              fullSymbol={<img src={fullStar} className="icon" />}
+            />
+          </div> */}
           <div className="fixer__rating">
             <img src={fullStar} alt="star" className="fixer__rating-star" />
             <img src={fullStar} alt="star" className="fixer__rating-star" />
