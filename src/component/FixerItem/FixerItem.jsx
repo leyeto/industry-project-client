a4e00e8da3b4c92981b7fd015c20db6a4b4d6fe1
Joe Shandley <josephshandley@gmail.com>
2022-07-29 02:46:07 +0100

    Add conditional profile pic rendering

--- src/component/FixerItem/FixerItem.jsx
+++ src/component/FixerItem/FixerItem.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./FixerItem.scss";
+import image1 from "../../assets/images/profile-image.jpg";
+import image2 from "../../assets/images/blonde.jpg";
+import image3 from "../../assets/images/omid-armin-UVx7Xx_b4a0-unsplash.jpg";
 
 export default function FixerItem({ fixer }) {
   const distance = Math.round(fixer.distance * 100) / 100;
@@ -7,7 +10,24 @@ export default function FixerItem({ fixer }) {
   return (
     <li className="fixer">
       {/* <Link to={`/fixer/${fixer.id}`}> */}
-      <div className="fixer__image"></div>
+      <img
+        className={`fixer__image ${
+          fixer.id !== "1" ? "fixer__image--hide" : "fixer__image--show"
+        }`}
+        src={image1}
+      />
+      <img
+        className={`fixer__image ${
+          fixer.id !== "2" ? "fixer__image--hide" : "fixer__image--show"
+        }`}
+        src={image2}
+      />
+      <img
+        className={`fixer__image ${
+          fixer.id !== "3" ? "fixer__image--hide" : "fixer__image--show"
+        }`}
+        src={image3}
+      />
       <div className="fixer__text">
         <h3 className="fixer__name">{fixer.name}</h3>
         <p className="fixer__distance">{`${distance}km away`}</p>
