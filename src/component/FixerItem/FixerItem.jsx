194d0c8030386eb4bee712406fae741b34fdbe81
Joe Shandley <josephshandley@gmail.com>
2022-07-29 02:52:56 +0100

    Style rough version of fixer list items

--- src/component/FixerItem/FixerItem.jsx
+++ src/component/FixerItem/FixerItem.jsx
@@ -10,24 +10,26 @@ export default function FixerItem({ fixer }) {
   return (
     <li className="fixer">
       {/* <Link to={`/fixer/${fixer.id}`}> */}
-      <img
-        className={`fixer__image ${
-          fixer.id !== "1" ? "fixer__image--hide" : "fixer__image--show"
-        }`}
-        src={image1}
-      />
-      <img
-        className={`fixer__image ${
-          fixer.id !== "2" ? "fixer__image--hide" : "fixer__image--show"
-        }`}
-        src={image2}
-      />
-      <img
-        className={`fixer__image ${
-          fixer.id !== "3" ? "fixer__image--hide" : "fixer__image--show"
-        }`}
-        src={image3}
-      />
+      <div className="fixer__image-container">
+        <img
+          className={`fixer__image ${
+            fixer.id !== "1" ? "fixer__image--hide" : "fixer__image--show"
+          }`}
+          src={image1}
+        />
+        <img
+          className={`fixer__image ${
+            fixer.id !== "2" ? "fixer__image--hide" : "fixer__image--show"
+          }`}
+          src={image2}
+        />
+        <img
+          className={`fixer__image ${
+            fixer.id !== "3" ? "fixer__image--hide" : "fixer__image--show"
+          }`}
+          src={image3}
+        />
+      </div>
       <div className="fixer__text">
         <h3 className="fixer__name">{fixer.name}</h3>
         <p className="fixer__distance">{`${distance}km away`}</p>
